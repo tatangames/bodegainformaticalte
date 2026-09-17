bb7a01a4f6522b7eb60ca9d1315ea2c8a2ff0917
Tatan <tatangamess@gmail.com>
2026-09-17 10:47:56 -0600

    reporte

--- resources/views/backend/admin/reportes/global/vistaredireccionsistema.blade.php
+++ resources/views/backend/admin/reportes/global/vistaredireccionsistema.blade.php
@@ -313,6 +313,17 @@
             </span>
         </a>
 
+        {{-- Sistema 14 --}}
+        <a href="http://190.86.196.105/centroformacion.com/" target="_blank" rel="noopener noreferrer" class="sys-card">
+            <div class="sys-icon ic-teal">
+                <i class="ti ti-settings" aria-hidden="true"></i>
+            </div>
+            <p class="sys-name">Centro de Formación</p>
+            <span class="sys-link">
+                <i class="ti ti-external-link"></i> Ingresar
+            </span>
+        </a>
+
 
 
     </div>
